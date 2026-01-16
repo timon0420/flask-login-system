09b171c9a95cc0325b126b354267db468615ae3b
Szymon Mędela <medelaszymon@gmail.com>
2026-01-16 11:24:42 +0100

    .

--- src/__init__.py
+++ src/__init__.py
@@ -12,7 +12,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-from src.models import User
+from src.models import User, Task
 
 @login_manager.user_loader
 def load_user(id):
